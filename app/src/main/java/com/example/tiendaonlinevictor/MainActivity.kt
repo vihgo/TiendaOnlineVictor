a7a8e1c6ca6b9ed8dafba0e703f64a9bb661f2a2
vihgo <vi.gomeza@profesor.duoc.cl>
2025-09-24 21:31:01 -0300

    Se agregó un componente para crear una tarjeta con un mensaje.

--- app/src/main/java/com/example/tiendaonlinevictor/MainActivity.kt
+++ app/src/main/java/com/example/tiendaonlinevictor/MainActivity.kt
@@ -4,44 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.tiendaonlinevictor.model.Mensaje
+
 import com.example.tiendaonlinevictor.ui.theme.TiendaOnlineVictorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            TiendaOnlineVictorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            //TiendaOnlineVictorTheme
+            TarjetaConMensaje(
+                Mensaje("Viktor","Bienvenido a la primera clase de Jetpack Compose")
+            )
+
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TiendaOnlineVictorTheme {
-        Greeting("Android")
+fun TarjetaConMensaje(mensaje: Mensaje){
+    Row(modifier = Modifier.padding(all=10.dp)) {
+        Image(
+            painter = painterResource(R.drawable.zoroicon),
+            contentDescription = "Imagen de perfil",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text(mensaje.autor)
+            Spacer(modifier = Modifier.height(7.dp))
+            Text(mensaje.cuerpo)
+        }
     }
+
+
+}
+@Preview
+@Composable
+fun VistaPreviaTarjetaConMensaje(){
+    TarjetaConMensaje( Mensaje("Viktor","Bienvenido a la primera clase de Jetpack Compose"))
 }
---
+++ app/src/main/java/com/example/tiendaonlinevictor/model/Mensaje.kt
@@ -0,0 +1,3 @@
+package com.example.tiendaonlinevictor.model
+
+data class Mensaje(val autor:String,val cuerpo:String)
